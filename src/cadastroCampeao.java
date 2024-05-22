6c49b10b8aa3da79adbbcfdf4c36c9b60e139e9d
Joao <joaovitordeoliveirasilva79@gmail.com>
2024-05-22 11:28:05 -0300

    adicionei um cadastro de um campeao

---
+++ src/cadastroCampeao.java
@@ -0,0 +1,36 @@
+public class cadastroCampeao {
+    String nomedocampeao;
+    private String rota;
+    private String classe;
+    public cadastroCampeao(String nomedocampeao, String rota, String classe){
+        this.nomedocampeao = nomedocampeao;
+
+        this.rota= rota;
+
+        this.classe = classe;
+    }
+
+    public String getNomedocampeao() {
+        return nomedocampeao;
+    }
+
+    public void setNomedocampeao(String nomedocampeao) {
+        this.nomedocampeao = nomedocampeao;
+    }
+
+    public String getRota() {
+        return rota;
+    }
+
+    public void setRota(String rota) {
+        this.rota = rota;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+}
